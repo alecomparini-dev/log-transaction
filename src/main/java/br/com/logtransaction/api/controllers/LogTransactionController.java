7b360a691918d95c6037f0a3386cdcd274949ff5
alecomparini-dev <alecomparini.dev@gmail.com>
2022-02-04 17:17:52 -0300

    fix: unused imports

--- src/main/java/br/com/logtransaction/api/controllers/LogTransactionController.java
+++ src/main/java/br/com/logtransaction/api/controllers/LogTransactionController.java
@@ -1,18 +1,17 @@
 package br.com.logtransaction.api.controllers;
 
-import br.com.logtransaction.api.models.LogTransaction;
-import br.com.logtransaction.api.services.LogTransactionService;
-import br.com.logtransaction.api.services.exceptions.BadRequestException;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import br.com.logtransaction.api.models.LogTransaction;
+import br.com.logtransaction.api.services.LogTransactionService;
+import br.com.logtransaction.api.services.exceptions.BadRequestException;
 
 @RestController
 public class LogTransactionController {
